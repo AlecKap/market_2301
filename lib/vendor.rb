b1808842e287e6bf494f335eade4626cf9f5de1d
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-03-07 09:54:39 -0700

    Feat: add methods for vendor class.

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,3 +1,17 @@
 class Vendor
-  
+  attr_reader :name,
+              :inventory
+
+  def initialize(name)
+    @name = name
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    @inventory[item.name]
+  end
+
+  def stock(item, quantity)
+    @inventory[item.name] += quantity
+  end
 end
